052a93407acfe70c659038764231ff078bd30543
Huawei Chen <huawei.chen@xvt.com.au>
2017-12-11 20:43:31 +1000

    notice board ver 1

--- src/Board.js
+++ src/Board.js
@@ -6,31 +6,89 @@ class Board extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			notes: []
+			notices: [
+				{id: 0, text: 'Notice 0'},
+				{id: 1, text: 'Notice 1'},
+				{id: 2, text: 'Notice 2'},
+				{id: 3, text: 'Notice 3'},
+				{id: 4, text: 'Notice 4'},
+			]
 		}
+		this.eachNotice = this.eachNotice.bind(this)
+		this.onChange = this.onChange.bind(this)
+		this.onDelete = this.onDelete.bind(this)
+		this.nextId = this.nextId.bind(this)
 	}
 
 	/**
-	 * Display each node onto board.
+	 * Display each notice onto board.
 	 */
-	eachNode(note) {
+	eachNotice(notice) {
+		return (
+				<Notice key={notice.id}
+								id={notice.id}
+								onChange={this.onChange}
+								onDelete={this.onDelete}>
+					{notice.text}
+				</Notice>
+		)
+	}
 
+	onChange(newNotice, id) {
+		let notices = this.state.notices.map(
+				notice => {
+				if (notice.id !== id) {
+					return notice;
+				}
+				else {
+					return {
+						...notice,
+						text: newNotice
+					}
+				}
+			}
+		)
+		this.setState({notices})
+	}
+
+	onDelete(noticeId) {
+		let notices = this.state.notices.filter(
+				notice => {
+					return notice.id !== noticeId
+				}
+		);
+		this.setState({notices})
+	}
+
+	/**
+	 * Get new notice ID.
+	 * @returns {Number}
+	 */
+	nextId() {
+		return this.state.notices.length;
 	}
 
 	/**
 	 * Add new notice to the board.
 	 */
-	addNew() {
-
+	addNew(text) {
+		let notices = [
+				...this.state.notices,
+			{
+				id: this.nextId(),
+				text: text
+			}
+		]
+		this.setState({notices});
 	}
 
 	render() {
 		return (
 				<div className='board'>
-					{this.state.notes}
-					<button onClick={() => {
-						console.log('addNew')
-					}}>
+					{this.state.notices.map(this.eachNotice)}
+					<button onClick={
+						() => this.addNew('New notice')
+					}>
 						Add
 					</button>
 				</div>
--- src/Notice.js
+++ src/Notice.js
@@ -1,7 +1,107 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Draggable from 'react-draggable';
 
 class Notice extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			editing: false
+		}
+		this.save = this.save.bind(this)
+		this.edit = this.edit.bind(this)
+		this.remove = this.remove.bind(this)
+	}
+
+	componentWillMount() {
+		/**
+		 * Randomize the notice initial position.
+		 * @type {{right: *, top: *}}
+		 */
+		this.position = {
+			right: this.randomPosition(0, window.innerWidth - 200, 'px'),
+			top: this.randomPosition(0, window.innerHeight - 200, 'px'),
+		}
+	}
+
+	/**
+	 * Get a random position with given range and unit.
+	 * @param from
+	 * @param to
+	 * @param unit
+	 * @returns {*}
+	 */
+	randomPosition(from, to, unit) {
+		return (from + Math.ceil(Math.random() * (to - from))) + unit;
+	}
+
+	/**
+	 * Save a notice.
+	 */
+	save() {
+		this.props.onChange(this.refs.newNotice.value, this.props.id)
+		this.setState({
+			editing: false
+		})
+	}
+
+	/**
+	 * Notice in editing state.
+	 */
+	edit() {
+		this.setState({
+			editing: true
+		})
+	}
+
+	/**
+	 * Remove a notice.
+	 */
+	remove() {
+		this.props.onDelete(this.props.id)
+	}
+
+	/**
+	 * Render notice as a textarea.
+	 */
+	renderForm() {
+		return (
+				<div className='notice'
+				     style={this.position}>
+					<textarea ref='newNotice'
+					          defautValue={this.props.children}>
+					</textarea>
+					<button onClick={this.save}>Save</button>
+				</div>
+		)
+	}
+
+	/**
+	 * Render notice as a paragraph.
+	 */
+	renderView() {
+		return (
+				<div className='notice'
+				     style={this.position}>
+					<p>{this.props.children}</p>
+					<span>
+						<button onClick={this.edit}>Edit</button>
+						<button onClick={this.remove}>Remove</button>
+					</span>
+				</div>
+		)
+	}
+
+	render() {
+		return (
+				<Draggable>
+					{(this.state.editing) ? this.renderForm()
+																: this.renderView()
+					}
+				</Draggable>
+		);
+	}
+
 }
 
 export default Notice;
